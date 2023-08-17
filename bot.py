7d65ec6914d7f0817c02b7296f62c953da7fa1aa
Khrish <106378292+SpidyKhrish007@users.noreply.github.com>
2023-08-17 21:42:53 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,27 +1,10 @@
-@@ -1,10 +1,4 @@
 import logging
 import logging.config
 
 # Get logging configurations
 logging.config.fileConfig('logging.conf')
 logging.getLogger().setLevel(logging.INFO)
-logging.getLogger("pyrogram").setLevel(logging.ERROR)import asyncio
-import logging
-import os
-import sys
-	@@ -35,6 +29,7 @@ async def start(self):
-        temp.U_NAME = me.username
-        temp.B_NAME = me.first_name
-        self.username = '@' + me.username
-        logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
-        logging.info(LOG_STR)
-        logging.info(script.LOGO)
-	@@ -45,109 +40,4 @@ async def start(self):
-        await self.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(today, time))
-        app = web.AppRunner(await web_server())
-        await app.setup()
-        bind_address = "0.0.0.0"
-        await web.
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("imdbpy").setLevel(logging.ERROR)
 
 from pyrogram import Client, __version__
@@ -39,7 +22,7 @@ from aiohttp import web
 from plugins import web_server
 
 class Bot(Client):
-
+	
     def __init__(self):
         super().__init__(
             name=SESSION,
@@ -50,7 +33,7 @@ class Bot(Client):
             plugins={"root": "plugins"},
             sleep_threshold=5,
         )
-
+	    
     async def start(self):
         b_users, b_chats = await db.get_banned()
         temp.BANNED_USERS = b_users
@@ -74,11 +57,11 @@ class Bot(Client):
         await app.setup()
         bind_address = "0.0.0.0"
         await web.TCPSite(app, bind_address, PORT).start()
-
+	    
     async def stop(self, *args):
         await super().stop()
         logging.info("Bot stopped. Bye.")
-
+	    
     async def iter_messages(
         self,
         chat_id: Union[int, str],
@@ -117,6 +100,7 @@ class Bot(Client):
             for message in messages:
                 yield message
                 current += 1
+
 
 app = Bot()
 app.run()
