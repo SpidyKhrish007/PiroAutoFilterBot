bfeac6b129725085cba20475c2194ccbb6276863
Khrish <106378292+SpidyKhrish007@users.noreply.github.com>
2023-08-19 16:27:11 +0530

    Update info.py

--- info.py
+++ info.py
@@ -12,7 +12,7 @@ def is_enabled(value, default):
         return default
 
 # Bot information
-PORT = environ.get("PORT", "4580")
+PORT = environ.get("PORT", "8888")
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '23830477'))
 API_HASH = environ.get('API_HASH', '19f8365d98fb11c9cd6c1eaa8b1fa4b8')
